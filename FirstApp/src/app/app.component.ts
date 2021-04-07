1f6cef0713c23997ae30a07bce12523c4da97cb1
gshapiev <gshapiev@yahoo.com>
2021-04-07 14:45:16 -0700

    initial

--- FirstApp/src/app/app.component.ts
+++ FirstApp/src/app/app.component.ts
@@ -10,4 +10,15 @@ export class AppComponent {
   title = 'FirstApp';
   @ViewChild('terminal1') terminal;
 
+  async run() {
+    let v1 = await this.terminal.ReadLine("Enter Value 1: ");
+    if(isNaN(v1))
+    {
+      this.terminal.WriteLine("Not a number")
+      return;
+    }
+    let v2 = await this.terminal.ReadLine("Enter Value 2: ");
+    this.terminal.WriteLine(v1 + v2);
+  }
+
 }
--- FirstApp/src/app/terminal/terminal.component.ts
+++ FirstApp/src/app/terminal/terminal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, Input, OnInit, ViewChild, ElementRef, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-terminal',
@@ -14,16 +14,65 @@ export class TerminalComponent implements OnInit {
   constructor() { 
   }
   private _body = "";
-  get body() { return this._body; }
+  get body() { return this._body + this._input + (this._inputStarted ? "|" : ""); }
 
   ngOnInit(): void {
     this._body = " > "
   }
 
-  print(t) {
+  WriteLine(t) {
     this._body += t + "\n > ";
     this.scrollToBottom();
   }
+
+  Write(t) {
+    this._body += t;
+    this.scrollToBottom();
+  }
+
+
+  private _input = "";
+  private _inputStarted = false;
+  async ReadLine(t) {
+    this.Write(t);
+    this._input = "";
+    var input = await this.getUserInput(this)
+    this._input = "";
+    this.WriteLine(input)
+    return input;
+  }
+
+  getUserInput(target) {
+    target._input = "|";
+    var isStarted = true;
+    var inputString = "";
+    return new Promise((resolve, reject) => {
+      window.onkeypress = function(e) {
+        if(isStarted) {
+          if(e.keyCode == 13) {
+            isStarted = false;
+            resolve(inputString);
+            return false;
+          } else {
+            inputString += e.key;
+            target._input = inputString + "|";
+          }
+        }
+      };
+    });
+  }
+
+  @HostListener('window:keypress', ['$event'])
+  onKeyPress(event) {
+    if(this._inputStarted) {
+      if(event.keyCode == 13) {
+        this._inputStarted = false;
+      } else {
+        this._input += event.key;
+      }
+    }
+  }
+  
   
   clear() {
     this._body = " > ";
@@ -37,4 +86,16 @@ export class TerminalComponent implements OnInit {
       behavior: 'smooth'
     });
   }
+
+  async doTimer() {
+    while(this._inputStarted) {
+        await delay(1000);
+    }
+  }
+}
+
+function delay(delay: number) {
+  return new Promise(r => {
+      setTimeout(r, delay);
+  })
 }
